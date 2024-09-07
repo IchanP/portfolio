93d7df0319eb94e38257d8792c2f73f4a13e11c6
IchanP <140890020+IchanP@users.noreply.github.com>
2024-09-07 08:28:32 +0200

    Initial commit

---
+++ src/app/components/ThemeSwitcher.tsx
@@ -0,0 +1,32 @@
+// Code from: https://joelolawanle.com/posts/light-dark-mode-nextjs-app-router-tailwind
+'use client';
+
+import { useState, useEffect } from 'react';
+import { useTheme } from 'next-themes';
+import { Moon, Sun } from 'lucide-react';
+
+const ThemeSwitcher = () => {
+    const [mounted, setMounted] = useState(false);
+    const { theme, setTheme, resolvedTheme } = useTheme();
+
+    useEffect(() => {
+        setMounted(true);
+    }, []);
+
+    if (!mounted) {
+        return null;
+    }
+    return (
+        <div className="bg-background text-primary-green absolute pt-2 pl-2">
+            <button
+                onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
+                className="p-2 rounded-full bg-gray-200 dark:bg-gray-800 hover:bg-gray-300 dark:hover:bg-gray-700 transition-colors"
+                aria-label="Toggle Dark Mode"
+            >
+                {theme === 'dark' ? <Moon className="text-yellow-500" /> : <Sun className="text-black-500" />}
+            </button>
+        </div>
+    );
+};
+
+export default ThemeSwitcher;
