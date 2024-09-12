cbc53dd460dede3e7a0bbfb92ea1ca4cc90af66f
Pontus Grandin <smeaurix@gmail.com>
2024-09-12 18:19:51 +0200

    re-enable linting on build

--- src/components/SocialMedia.tsx
+++ src/components/SocialMedia.tsx
@@ -3,10 +3,15 @@ import IconText from "./ui/IconText";
 
 const SocialMedia = () => {
   const t = useTranslations("SocialMedia");
+  // TODO: Implement social media links
   return (
     <div className="flex flex-col gap-1 items-center md:flex-row">
       <h2 className="text-l">{t("find_me")}</h2>
-      <IconText iconSource="/ln-blue-40.png" alt="LinkedIn" />
+      <div className="flex flex-row gap-2 items-center">
+        <IconText iconSource="/ln-blue-40.png" alt="LinkedIn" />
+        <IconText iconSource="/ln-blue-40.png" alt="Gmail" />
+        <IconText iconSource="/ln-blue-40.png" alt="GitHub" />
+      </div>
     </div>
   );
 };
