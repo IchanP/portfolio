eab3ee65bd00845b7199c5bfa51b14d881114fda
Pontus Grandin <smeaurix@gmail.com>
2024-10-02 22:53:45 +0200

    styling

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export default function Home() {
   const t = useTranslations("HomePage");
   return (
-    <div className="grid grid-rows-2 pt-5 gap-5 ml-5 mr-5 md:grid-rows-0 md:grid-cols-2 md:ml-20">
+    <div className="grid grid-rows-[1fr_0.5fr] pt-5 gap-5 ml-5 mr-5 md:grid-rows-0 md:grid-cols-2 md:ml-20">
       <div>
         <h1 className="text-3xl text-center font-courier-prime">
           {t("About")}
