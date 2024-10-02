bdf386b2f5d868efe73558aded4f0ba74389c6d8
Pontus Grandin <smeaurix@gmail.com>
2024-10-02 14:32:21 +0200

    menu borders

--- src/components/ui/MenuDiv.tsx
+++ src/components/ui/MenuDiv.tsx
@@ -14,7 +14,7 @@ const MenuDiv = ({
   return (
     <div
       className={classNames(
-        "h-12 flex flex-col justify-center items-center w-full text-center bg-lightPrimary hover:bg-lightHover dark:bg-darkPrimary dark:hover:bg-darkHover md:w-auto",
+        "h-12 border-t-4 border-[#fcf2f2] dark:border-[#0d0f14] flex flex-col justify-center items-center w-full text-center bg-lightPrimary hover:bg-lightHover dark:bg-darkPrimary dark:hover:bg-darkHover md:border-t-0 md:w-auto",
         hoverColors,
       )}
     >
