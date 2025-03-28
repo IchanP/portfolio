aaed7628eb003882949aa9aff73c72a2e8aabbe0
Pontus <smeaurix@gmail.com>
2025-03-28 18:36:42 +0100

    add mobile state hook

--- src/components/ui/HoverableImage.tsx
+++ src/components/ui/HoverableImage.tsx
@@ -1,6 +1,8 @@
 "use client";
+import classNames from "classnames";
 import Image from "next/image";
 import { useState } from "react";
+import { useIsDarkmode } from "stores/DarkModeState";
 
 const HoverableImage = ({
   src,
@@ -12,21 +14,45 @@ const HoverableImage = ({
   onError: () => void;
 }) => {
   const [displayAlt, setDisplayAlt] = useState(false);
+  const [saturation, setSaturation] = useState("saturate-50");
+
+  const onHover = () => {
+    setDisplayAlt(true);
+    setSaturation("saturate-100");
+  };
+
+  const onLeave = () => {
+    setDisplayAlt(false);
+    setSaturation("saturate-50");
+  };
+
   return (
     <div
       className="relative w-full"
-      onMouseLeave={() => setDisplayAlt(false)}
-      onMouseOver={() => setDisplayAlt(true)}
+      onMouseLeave={() => onLeave()}
+      onMouseOver={() => onHover()}
     >
       <div
-        className={`text-xs bg-zinc-800 rounded-md absolute font-lexend -top-6 font-bold text-center -right-5 transition-opacity duration-200 inline-flex items-center justify-center ${
-          displayAlt ? "opacity-90" : "opacity-0"
-        }`}
+        className={`text-xs bg-zinc-800 rounded-md flex flex-row justify-center
+      items-center absolute font-lexend -top-6 font-bold
+      text-center transition-opacity duration-200 inline-flex items-center justify-center
+      left-1/2 transform -translate-x-1/2 ${
+        displayAlt ? "opacity-90" : "opacity-0"
+      }`}
       >
-        <p className="p-[3px] break-words w-[50px]">{alt}</p>
+        <p className="p-[3px] no-wrap max-w-[100px] whitespace-nowrap overflow-hidden text-ellipsis">
+          {alt}
+        </p>
       </div>
 
-      <Image src={src} alt={alt} width={70} height={40} onError={onError} />
+      <Image
+        src={src}
+        alt={alt}
+        width={70}
+        height={40}
+        onError={onError}
+        className={classNames("ease-in-out duration-300", saturation)}
+      />
     </div>
   );
 };
--- src/data/techStack.ts
+++ src/data/techStack.ts
@@ -2,34 +2,58 @@ export const externalIcons: ExternalIconLink[] = [
   {
     href: "https://google.com",
     src: {
-      darkmode: "/github-mark.svg",
-      lightmode: "/github-mark.svg",
+      darkmode: "/stack/ts-logo-128.svg",
+      lightmode: "/stack/ts-logo-128.svg",
     },
     alt: "TypeScript",
   },
   {
     href: "https://google.com",
     src: {
-      darkmode: "/github-mark.svg",
-      lightmode: "/github-mark.svg",
+      darkmode: "/stack/mysql.svg",
+      lightmode: "/stack/mysql.svg",
     },
-    alt: "JavaScript",
+    alt: "MySQL",
   },
   {
     href: "https://google.com",
     src: {
-      darkmode: "/github-mark.svg",
-      lightmode: "/github-mark.svg",
+      darkmode: "/stack/css.svg",
+      lightmode: "/stack/css.svg",
     },
-    alt: "Github",
+    alt: "CSS",
   },
   {
     href: "https://google.com",
     src: {
-      darkmode: "/github-mark.svg",
-      lightmode: "/github-mark.svg",
+      darkmode: "/stack/docker.svg",
+      lightmode: "/stack/docker.svg",
     },
-    alt: "Visual Studio",
+    alt: "Docker",
+  },
+  {
+    href: "https://google.com",
+    src: {
+      darkmode: "/stack/kubernetes.svg",
+      lightmode: "/stack/kubernetes.svg",
+    },
+    alt: "Kubernetes",
+  },
+  {
+    href: "https://google.com",
+    src: {
+      darkmode: "/stack/react.svg",
+      lightmode: "/stack/react.svg",
+    },
+    alt: "React",
+  },
+  {
+    href: "https://google.com",
+    src: {
+      darkmode: "/stack/tailwind.svg",
+      lightmode: "/stack/tailwind.svg",
+    },
+    alt: "Tailwind",
   },
 ];
 
