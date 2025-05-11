4a8383c4d4d0fac51c254276158d8c290322909d
Pontus <smeaurix@gmail.com>
2025-05-11 10:41:01 +0200

    fix base url

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: process.env.NEXT_PUBLIC_SITE_URL as string,
+      url: process.env.NEXT_PUBLIC_BASE_URL as string,
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 1,
